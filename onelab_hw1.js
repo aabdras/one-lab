c6eedcd7d6a8c00fcc6aa29ccf95b61f22e21b6c
Aizat <61237983+aabdras@users.noreply.github.com>
2021-11-01 12:19:01 +0600

    Add files via upload

---
+++ onelab_hw1.js
@@ -0,0 +1,41 @@
+const numbersList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const cities = [
+  { name: 'Almaty', code: 'ALA' },
+  { name: 'Nursultan', code: 'AST'},
+  { name: 'Shymkent', code: 'SHY' },
+  { name: 'Aktau', code: 'AKT' },
+  { name: 'Atyrau', code: 'ATY' },
+  { name: 'Karaganda', code: 'KAR' },
+  { name: 'Kyzylorda', code: 'KZY' },
+  { name: 'Pavlodar', code: 'PAV' },
+];
+
+console.log('Четные числа: '+numbersList.filter(x=>x%2==0))
+
+console.log('Нечетные числа: '+numbersList.filter(x=>x%2==1))
+
+console.log('Список городов, которые начинаются с буквы K:')
+console.log(cities.filter(x => x.name.substr(0,1)=='K'))
+
+console.log('Названия городов: ')
+cities.map(x=>console.log(x.name+''))
+
+sum=1;
+for(let i=0; i<numbersList.length; i++)
+{
+    sum*=numbersList[i]
+}
+console.log('Произведение элементов: '+sum)
+
+console.log('Произведение элементов: '+numbersList.reduce((total,amount)=>total*amount))
+
+console.log('Cумма элементов: '+numbersList.reduce((total,amount)=>total+amount))
+
+let city=new Object();
+cities.reduce(function(start,end)
+{
+   city[end.code]=end.name
+})
+
+console.log('Объект cities: ')
+console.log(city)
